f29efbbdc249d4684885efb277a50bd283afb212
@khad0062 <khad0062@algonquinlive.com>
2025-05-25 21:04:11 -0400

    feat: complete Hybrid-H03 — secure weather API key and integrate Redis cache with Azure deployment

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,6 +3,7 @@ import * as resources from '@pulumi/azure-native/resources'
 import * as containerregistry from '@pulumi/azure-native/containerregistry'
 import * as dockerBuild from '@pulumi/docker-build'
 import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as cache from '@pulumi/azure-native/redis'
 
 
 // Import the configuration settings for the current stack.
@@ -33,6 +34,7 @@ const registry = new containerregistry.Registry(`${prefixName}ACR`, {
 })
 
 
+
 // Get the authentication credentials for the container registry.
 const registryCredentials = containerregistry
   .listRegistryCredentialsOutput({
@@ -45,6 +47,33 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
+
+  // Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: 'westus3',
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: 'Latest',
+  minimumTlsVersion: '1.2',
+  redisConfiguration: {
+    maxmemoryPolicy: 'allkeys-lru'
+  },
+  sku: {
+    name: 'Basic',
+    family: 'C',
+    capacity: 0
+  }
+})
+
+  // Extract the auth creds from the deployed Redis service
+const redisAccessKey = cache.listRedisKeysOutput({
+  name: redis.name,
+  resourceGroupName: resourceGroup.name,
+}).apply((keys: { primaryKey: string }) => keys.primaryKey);
+
+// Construct the Redis connection string to be passed as an environment variable in the app container
+const redisConnectionString = pulumi.interpolate`rediss://:${redisAccessKey}@${redis.hostName}:${redis.sslPort}`;
 
 
 
@@ -95,8 +124,12 @@ const containerGroup = new containerinstance.ContainerGroup(
           },
           {
             name: 'WEATHER_API_KEY',
-            value: '32c34ad2a0c141d1e51763c71eb249ff',
+            value: config.requireSecret('weatherApiKey')
           },
+          {
+            name: 'REDIS_URL',
+            value: redisConnectionString
+          }
         ],
         resources: {
           requests: {
